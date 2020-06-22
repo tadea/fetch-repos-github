1e39eaa4e804773bf50e9e45e0fdc926ed1f67c4
Tadea <tsimunoviczg@gmail.com>
2020-06-22 14:29:45 -0400

    inital commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "Iv1.ff1ff4b2374e8137";
+const client_secret = "2bebafba0f31f9dbbcc8c8015bde62d7255535e8";
+
+searchButton.addEventListener("click", () => {
+  console.log("hello world");
+});
